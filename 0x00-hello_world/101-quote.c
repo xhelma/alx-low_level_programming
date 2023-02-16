cf83da20100f3d16c93a22feae4f6941abbc93c1
xhelma <97184575+xhelma@users.noreply.github.com>
2023-02-16 15:22:37 -0800

    fixing typo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,5 +8,7 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	str message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, message);
 	return (1);
