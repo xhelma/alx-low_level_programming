0b9c2edd706f3d0df7446c21765fc5911f40d169
xhelma <97184575+xhelma@users.noreply.github.com>
2023-02-17 15:05:20 -0800

    adding program 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+					if (i == 7)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
