bf3d75aa583ca937dcd83fa5326281ea5bd0654d
xhelma <97184575+xhelma@users.noreply.github.com>
2023-02-17 11:32:11 -0800

    adding program 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	return (0);
+}
