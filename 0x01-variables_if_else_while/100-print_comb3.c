9af1c0045c8243f06c6453877f649f20606561c2
xhelma <97184575+xhelma@users.noreply.github.com>
2023-02-17 13:30:17 -0800

    adding program 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,12 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		putchar('0' + i);
 		if (i == 9)
 			break;
 		for (j = 0; j <= 9; j++)
 		{
 			if (j > i)
+				putchar('0' + i);
 				putchar('0' + j);
 		}
 		putchar(',');
