de88a84c5e9fb0d099b5da8fffd2785dad2c8fa6
xhelma <97184575+xhelma@users.noreply.github.com>
2023-02-17 12:27:19 -0800

    adding program 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(letter);
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
