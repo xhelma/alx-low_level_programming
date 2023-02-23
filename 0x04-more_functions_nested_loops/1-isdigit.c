e4536973a41acc3ed42205aee0d54c4bb38a9482
xhelma <97184575+xhelma@users.noreply.github.com>
2023-02-23 13:02:56 +0100

    adding program 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for a digit (0 through 9).
+ *
+ * @c: the character to check.
+ *
+ * Return: 1 if @c is a digit.
+ *         0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	else
+		return (0);
+}
