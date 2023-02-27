c3c8cbb7169db6422117bbe3c7c7667343fc307a
xhelma <97184575+xhelma@users.noreply.github.com>
2023-02-27 23:42:20 +0100

    adding program 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+
+
+/**
+ * rev_string - a function that prints a string,
+ *             in reverse, followed by a new line.
+ *
+ * @s: a string
+ *
+ *
+ * Return: void.
+ *
+ */
+
+void rev_string(char *s)
+{
+	char *p = s;
+	char *r;
+	char rev[100];
+
+	r = rev;
+	while (*p)
+	{
+		p++;
+	}
+	for (p = p - 1; p >= s; p--)
+	{
+		*r = *p;
+		r++;
+	}
+
+	*r = '\0';
+	r = rev;
+
+	for (; *r != '\0'; r++)
+	{
+		*s = *r;
+		s++;
+	}
+}
