c11f69418a07ad4c6bc1a2f9d8b2cba1d672d9ed
xhelma <97184575+xhelma@users.noreply.github.com>
2023-02-28 00:45:30 +0100

    adding program 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+
+/**
+ * puts_half - a function that prints every other character of a string,
+ *	   starting with the first character, followed by a new line
+ *
+ * @str: a string
+ *
+ *
+ * Return: void.
+ *
+ */
+
+void puts_half(char *str)
+{
+	char *p = str;
+	int i = 0;
+	int half;
+
+	while (*p)
+	{
+		p++;
+		i++;
+	}
+
+	if (i % 2 == 0)
+		half = i / 2;
+	else
+		half = (i - 1) / 2;
+
+	p = str;
+	for (p = str + half; p < str + i; p++)
+	{
+		_putchar(*p);
+	}
+	_putchar(10);
+}
