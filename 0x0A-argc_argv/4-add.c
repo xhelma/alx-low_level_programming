6729938a44aa2a2ad7d0603cf4317964ede1c260
xhelma <97184575+xhelma@users.noreply.github.com>
2023-03-10 15:07:10 +0100

    adding another condition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if ((strtol(argv[i], &p, 10)) == 0)
+		if ((strtol(argv[i], &p, 10)) == 0 || (*p != '\0'))
 		{
 			printf("Error\n");
 			return (1);
