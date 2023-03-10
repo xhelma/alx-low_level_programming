05769c00d358bf5e6efc8d9effb0a4a7d93aff55
xhelma <97184575+xhelma@users.noreply.github.com>
2023-03-10 12:10:39 +0100

    adding new directory

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argunemt count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
