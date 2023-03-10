46a33d3de35b1182d36f769268006461e4009e10
xhelma <97184575+xhelma@users.noreply.github.com>
2023-03-10 14:30:06 +0100

    adding program 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argunemt count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ *         Always 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	char *p;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if ((strtol(argv[i], &p, 10)) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
