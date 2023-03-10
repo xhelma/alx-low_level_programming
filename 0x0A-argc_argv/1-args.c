b597032c87535c875a4d2d452410a975445e79f1
xhelma <97184575+xhelma@users.noreply.github.com>
2023-03-10 12:16:56 +0100

    adding program 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argunemt count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
